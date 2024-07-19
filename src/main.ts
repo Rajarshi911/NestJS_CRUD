fb1ff691a25cafc318c35ae06057b1cb637233ed
Raj9101 <21051237@kiit.ac.in>
2024-07-19 22:58:21 +0530

    commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Todo Application API')
+    .setDescription(
+      'API documentation for the Todo Application built with NestJS. This API allows users to create, read, update, and delete tasks, manage user authentication, and more. It demonstrates the use of NestJS features such as validation, authentication, and Swagger integration.',
+    )
+    .setVersion('1.0')
+    .addTag('Todos (Rajarshi Dey)', 'Endpoints related to managing todos')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
